bafbcd4ee7e83cb7dfbe1935193844165d434caa
Pratap <pratapabhangg1@gmail.com>
2025-09-07 00:35:30 +0530

    [FIX] handling scrolling and data retrieval issues

--- app/src/main/java/com/abhang/matchmate/data/remote/repository/UserDataRepositoryImpl.kt
+++ app/src/main/java/com/abhang/matchmate/data/remote/repository/UserDataRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.abhang.matchmate.data.remote.repository
 
+import android.util.Log
 import com.abhang.matchmate.data.local.model.UserData
 import com.abhang.matchmate.data.remote.network.INetworkService
 import com.abhang.matchmate.domain.repository.UserDataLocalRepository
@@ -13,31 +14,55 @@ class UserDataRepositoryImpl @Inject constructor(
     private val networkHelper: NetworkHelper
 ) : UserDataRepository {
 
-    override suspend fun getUserData(results: Int, page: Int, gender: String): List<UserData> {
-        return try{
-            val cnt = localRepository.getUserCount()
-            if(cnt > results*page){
-                localRepository.getUserData(results, page)
-            }else {
-                if(networkHelper.isNetworkAvailable()){
-                    val mResponse = networkApi.getUserData(results, page, gender)
-                    val localData = localRepository.checkIfUserExist(mResponse.results.map{ it.toUserData().userId} )
-                    if(localData.containsAll(mResponse.results.map { it.login.uuid })) {
-                        localRepository.getUserData(results, page)
-                    } else {
-                        val pData = mResponse.results.filter { it.login.uuid !in localData }
-                        localRepository.addUsers(pData.map { it.toUserData() })
-                        localRepository.getUserData(results, page)
-                    }
-                }else{
-                    localRepository.getUserData(results, page)
-                }
-            }
+    override suspend fun getUserData(pageSize: Int, pageNo: Int, gender: String): List<UserData> {
+        val offset = pageNo * pageSize
+        val localUsers = localRepository.getUserData(pageSize, offset)
+        val totalUsersInDb = localRepository.getUserCount()
+        val mData = if (localUsers.size == pageSize) {
+            // Enough data in DB
+            localUsers
+        } else {
+            // Not enough data, call API to fetch more
+            val apiPageSize = pageSize - localUsers.size
+            val apiPageNumber = pageNo
 
-        } catch (e: Exception) {
-            e.printStackTrace()
-            localRepository.getUserData(results, page)
+            val apiUsers = networkApi.getUserData(apiPageSize, apiPageNumber, gender)
+            val pData = apiUsers.results.filter { it.login.uuid !in localUsers.map { it.userId } }
+            localRepository.addUsers( pData.map { it.toUserData() })
+
+
+            Log.e("---->", localRepository.getUserCount().toString())
+            // Return combined data
+            localRepository.getUserData(pageSize, pageNo)
         }
+        return mData
+
+
+
+//        return try{
+//            val cnt = localRepository.getUserCount()
+//            if(cnt > pageSize*pageNo){
+//                localRepository.getUserData(pageSize, pageNo)
+//            }else {
+//                if(networkHelper.isNetworkAvailable()){
+//                    val mResponse = networkApi.getUserData(pageSize, pageNo, gender)
+//                    val localData = localRepository.checkIfUserExist(mResponse.results.map{ it.toUserData().userId} )
+//                    if(localData.containsAll(mResponse.results.map { it.login.uuid })) {
+//                        localRepository.getUserData(pageSize, pageNo)
+//                    } else {
+//                        val pData = mResponse.results.filter { it.login.uuid !in localData }
+//                        localRepository.addUsers(pData.map { it.toUserData() })
+//                        localRepository.getUserData(pageSize, pageNo)
+//                    }
+//                }else{
+//                    localRepository.getUserData(pageSize, pageNo)
+//                }
+//            }
+//
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//            localRepository.getUserData(pageSize, pageNo)
+//        }
 
 
     }
